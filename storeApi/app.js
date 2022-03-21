d61d0be7a2b6123b18e79d07f9a2177996d2da5c
MaironL <Mairon_488@outlook.com>
2022-03-21 17:57:02 -0400

    storeApi project finished

---
+++ storeApi/app.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+require('express-async-errors');
+
+const express = require('express');
+const app = express();
+
+const connectDB = require('./db/connect');
+const producsRouter = require('./routes/products');
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+//*
+//* middleware
+
+app.use(express.json());
+
+//*----------------------------------------------
+
+//*
+//* routes
+
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products</a> ');
+});
+app.use('/api/v1/products', producsRouter);
+
+//*----------------------------------------------
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+//*----------------------------------------------
+
+//*
+//*  App starter
+
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`Server is listening in ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
